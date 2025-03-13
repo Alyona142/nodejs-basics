7b888887ccc1b80c49e6fc671d9f09b0a2d6d5e3
Alyona <olenapav97@gmail.com>
2025-03-13 13:42:24 +0200

    comm

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,13 +1,17 @@
 import createHttpError from 'http-errors';
 export const validateBody = (schema) => async (req, res, next) => {
   try {
-    await schema.validateAsinc(req.body, {
-      abortEarle: false,
+    console.log(req.body);
+
+    await schema.validateAsync(req.body, {
+      abortEarly: false,
     });
+    console.log('!before catch');
   } catch (err) {
     const error = createHttpError(404, 'Bad Request', {
       errors: err.details,
     });
+    console.log('!err', error);
     next(error);
   }
 };
--- src/validation/students.js
+++ src/validation/students.js
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 export const createStudentSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
-  age: Joi.number().integer().min(6).max(16).required(),
+  age: Joi.number().integer().min(6).max(90).required(),
   gender: Joi.string().valid('male', 'female', 'other').required(),
   avgMark: Joi.number().min(2).max(12).required(),
   onDuty: Joi.boolean(),
@@ -10,7 +10,7 @@ export const createStudentSchema = Joi.object({
 export const updateStudentSchema = Joi.object({
   name: Joi.string().min(3).max(30),
   email: Joi.string().email(),
-  age: Joi.number().integer().min(6).max(16),
+  age: Joi.number().integer().min(6).max(90),
   gender: Joi.string().valid('male', 'female', 'other'),
   avgMark: Joi.number().min(2).max(12),
   onDuty: Joi.boolean(),
